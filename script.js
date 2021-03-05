7e7ce76726306196b1c344ee84f4a4bf4ab20e1e
Michael Tai <michaeltaidev@gmail.com>
2021-03-05 11:42:01 -0800

    Adding starter project files.

---
+++ script.js
@@ -0,0 +1,7 @@
+var backgroundColor = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+
+color1.addEventListener("input", function outputColorText() {
+	console.log(color1.value);
+})
